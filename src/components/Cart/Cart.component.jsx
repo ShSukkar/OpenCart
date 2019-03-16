67dc5779de8c95bdd3da59c2589a31336d168b59
ShSukkar <sukkarsmiling89@hotmail.com>
2019-03-16 23:25:42 +0200

    Fetch cart content from localStorage

--- src/components/Cart/Cart.component.jsx
+++ src/components/Cart/Cart.component.jsx
@@ -2,7 +2,30 @@ import React, { Component } from 'react';
 import "./Cart.css";
 
 class Cart extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cartContent: []
+        }
+    }
+
+    componentDidMount() {
+        this.fetchCartContent();
+    }
+
+    //Get the contents of the cart, which were saved at the localStorage
+    fetchCartContent = () => {
+        let cartContent = Object.values(localStorage).map(product => {
+            return JSON.parse(product);
+        });
+
+        this.setState({ cartContent });
+    }
+
     render() {
+        // let cartContent = JSON.parse(window.localStorage.
+
         return (
             <div>
                 Cart
