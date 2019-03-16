73527fee54493e6cdaa2b330da8f0a8b6c63ff3a
ShSukkar <sukkarsmiling89@hotmail.com>
2019-03-17 01:23:30 +0200

    UPDATE basic structure of Checkout page

--- src/components/Checkout/Checkout.component.jsx
+++ src/components/Checkout/Checkout.component.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import "./Checkout.css";
+import Order from "./Order/Order.component";
 
 class Checkout extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
     render() {
         return (
-            <div className="Checkout">
-                Checkout
+            <div className="Checkout container">
+                <div className="row">
+                    <div className="col-md-8">
+                        INFO
+                    </div>
+                    <div className="col-md-4">
+                        <Order />
+                    </div>
+                </div>
             </div>
         );
     }
