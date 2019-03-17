864fc0aef727049efc77aee8eb625eff633e4d8f
ShSukkar <sukkarsmiling89@hotmail.com>
2019-03-17 11:18:51 +0200

    UPDATE Order component

--- src/components/Checkout/Order/Order.component.jsx
+++ src/components/Checkout/Order/Order.component.jsx
@@ -13,7 +13,8 @@ class Order extends Component {
         super(props);
 
         this.state = {
-            total: 0
+            total: 0,
+            payingType: "Cash"
         }
     }
 
@@ -27,6 +28,12 @@ class Order extends Component {
             total += Number(prod.price);
         });
         this.setState({ total });
+    }
+
+    changePaymentMethod = (e) => {
+        console.log(e.target);
+
+        this.setState({ payingType: e.target.value });
     }
 
     saveOrder = () => { }
@@ -46,44 +53,45 @@ class Order extends Component {
 
         return (
             <div className="Order">
-            <div className="orderDetails">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>PRODUCT</th>
-                            <th>PRICE</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {orderTableRows}
-                        <tr>
-                            <td>TOTAL</td>
-                            <td style={{ fontWeight: "bold" }}>{total} JD</td>
-                        </tr>
-                    </tbody>
-                </table>
-                {/* <Typography variant="p">PAYMENT METHOD</Typography> */}
-                <FormControl>
-                    <FormLabel component="legend">PAYMENT METHOD</FormLabel>
-                    <RadioGroup
-                        aria-label="PAYMENT METHOD"
-                        name="paymentMethod"
-                        value={this.state.value}
-                        onChange={this.handleChange}>
-                        <FormControlLabel
-                            value="Cash"
-                            control={<Radio color="primary" />}
-                            label="Cash"
-                            labelPlacement="end"
-                        />
-                        <FormControlLabel
-                            value="Stripe"
-                            control={<Radio color="primary" />}
-                            label="Stripe"
-                            labelPlacement="end"
-                        />
-                    </RadioGroup>
-                </FormControl>
+                <div className="orderDetails">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>PRODUCT</th>
+                                <th>PRICE</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {orderTableRows}
+                            <tr>
+                                <td>TOTAL</td>
+                                <td style={{ fontWeight: "bold" }}>{total} JD</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <FormControl style={{ marginLeft: "10px" }}>
+                        <FormLabel component="legend" color="primary">PAYMENT METHOD</FormLabel>
+                        <RadioGroup
+                            aria-label="PAYMENT METHOD"
+                            name="paymentMethod"
+                            color="primary"
+                            value={this.state.payingType}
+                            onChange={this.changePaymentMethod}>
+                            <FormControlLabel
+                                value="Cash"
+                                control={<Radio color="primary" />}
+                                label="Cash"
+                                labelPlacement="end"
+                            />
+                            <FormControlLabel
+                                value="Stripe"
+                                control={<Radio color="primary" />}
+                                label="Stripe"
+                                labelPlacement="end"
+                            />
+                        </RadioGroup>
+                    </FormControl>
                 </div>
                 < Button onClick={this.saveOrder} style={{ color: "white", backgroundColor: "darkorange", border: "none", width: "100%", height: "50px", letterSpacing: "1px", borderRadius: "20px", display: "block", margin: "auto" }}>PLACE ORDER</Button>
             </div>
